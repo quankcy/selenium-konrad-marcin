335ee78274dd5d48b4ce0c3cb148fd9c125ca22b
Karol Poznakowski <karol.poznakowski@gmail.com>
2023-04-02 10:42:10 +0200

    Selenium setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
     }
 }
