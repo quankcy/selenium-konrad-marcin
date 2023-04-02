42de7af6bc59099cbfef0047a849c127479f081b
Karol Poznakowski <karol.poznakowski@gmail.com>
2023-04-02 11:01:29 +0200

    Selenium setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,16 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://komputronik.pl");
     }
 }
