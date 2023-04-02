7b4f295231b22596ed53551d5c5db2a28ee932ff
Karol Poznakowski <karol.poznakowski@gmail.com>
2023-04-02 11:38:37 +0200

    First clicks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -11,8 +13,21 @@ public class Main {
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.addArguments("--disable-notifications");
 
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://komputronik.pl");
+
+        By.ByCssSelector zgadzamSieNaWszystkie = new By.ByCssSelector("#onetrust-accept-btn-handler");
+        driver.findElement(zgadzamSieNaWszystkie).click();
+
+        By.ByCssSelector followUpCloseButton = new By.ByCssSelector(".webpush-followup-close");
+        driver.findElement(followUpCloseButton).click();
+
+        By.ByCssSelector wsparcieKomputronikSelector = new By.ByCssSelector("a[href='https://www.komputronik.pl/informacje/uslugi/wsparcie-komputronik/']");
+        WebElement wsparcieKomputronik = driver.findElement(wsparcieKomputronikSelector);
+        wsparcieKomputronik.click();
+
+
     }
 }
